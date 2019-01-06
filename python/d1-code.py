c335ce3faad5e65445c7983672fd35a6332b88d1
Duke Lu <duke.lu@gmail.com>
2019-01-05 19:14:51 -0500

    Initial commit

---
+++ python/d1-code.py
@@ -0,0 +1,11 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(input("Enter integer: "))
+d2 = float(input("Enter a float: "))
+s2 = input("Enter a string: ")
+
+print(i+i2)
+print(d+d2)
+print(s + s2)
